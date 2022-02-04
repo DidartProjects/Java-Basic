ac412214b16f3e7170c61a02b0e9bc7618308ae1
DidartProjects <94858583+DidartProjects@users.noreply.github.com>
2022-02-05 01:10:53 +0200

    Add files via upload
    Projects Creation
    Write the program that an architect will need to prepare projects for several construction sites. The preparation of a project takes three hours.
    Entrance
    2 lines are read from the console:
    1. The name of the architect - text
    2. Number of projects - the whole number in the interval [0… 100]
    Exit
    The following is printed on the console:
    • "The architect {architect's name} will need {required hours} hours to complete {number of projects} project (s)."

---
+++ ProjectsCreation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int projectsCount = Integer.parseInt(scanner.nextLine());
+
+        int hours = projectsCount * 3;
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.", name, hours, projectsCount);
+    }
+}
+
+
+
